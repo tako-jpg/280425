3bbdcc7934360b7cb57d51b53e0c6895781de2ba
pt-kt <tany162@mail.ru>
2025-04-30 04:57:42 +0300

    начало

--- src/Main.java
+++ src/Main.java
@@ -26,9 +26,61 @@ public class Main {
         } else {
             System.out.println("Если у Вас другая скорость, узнайте о штрафах на сайте ГИБДД");
         }
-
+        System.out.println("Задача 4");
+        int age = 75;
+        if (age > 0 && age < 2) {
+            System.out.println("Вы слишком маленький - лучше остаться дома.");
+        }
+        if (age > 2 && age <= 6) {
+            System.out.println("Если возраст человека равен - " +age+ " то ему нужно ходить в детский сад.");
+        }
+        if (age > 7 && age <= 17) {
+            System.out.println("Если возраст человека равен - " +age+ " то ему нужно ходить в школу.");
+        }
+        if (age >= 18 && age <= 24) {
+            System.out.println("Если возраст человека равен - " +age+ " то ему нужно ходить в университет.");
+        }
+        if (age > 24 && age <= 99) {
+            System.out.println("В возрасте " + age + " лет можно работать.");
+        }
+        if (age >= 100) {
+            System.out.println("Вы слишком большой - лучше остаться дома.");
+        }
+        System.out.println("Задача 5");
+        int ageForCarousels = 15;
+        if (ageForCarousels < 5) {
+            System.out.println("Если возраст ребенка равен - " +ageForCarousels+ " г., то ему нельзя кататься на куруселях");
+        }
+        if (ageForCarousels > 5 && ageForCarousels < 14) {
+            System.out.println("Если возраст ребенка равен - " +ageForCarousels+ " лет, то ему можно кататься на каруселях в сопровождении взрослого");
+        }
+        if (ageForCarousels > 14) {
+            System.out.println("Если возраст ребенка равен - " + ageForCarousels+ " лет, то ему можно кататься на каруселях без сопровождения взрослых ");
+        }
+        System.out.println("Задача 6");
+        int passengerNumber = 102;
+        int remainingSeats = (60 - passengerNumber) + 1;
+        // int StandingSpace = 102 - 60; // 42 стоячих
+        int remainingStandingSpace = (102 - passengerNumber) + 1;
+        if (passengerNumber <= 60) {
+            System.out.println("Вы " + passengerNumber + " по счету, пассажир, можете приобрести сидячий билет, их осталось - " + remainingSeats);
+        } else if (passengerNumber >= 61 && passengerNumber <= 102) {
+            System.out.println("Вы " + passengerNumber + " по счету, пассажир, можете приобрести стоячее место, их осталось - " +remainingStandingSpace);
+        }
+        System.out.println("Задача 7");
+        int one = 55;
+        int two = 25;
+        int three = 11;
+        if (one > two && one > three) {
+            System.out.println("Число one больше остальных");
+        } else if (two > one && two > three) {
+            System.out.println("Число two больше остальных");
+        } else {
+            System.out.println("Число three больше остальных");
         }
     }
+    }
+
 
 
 
